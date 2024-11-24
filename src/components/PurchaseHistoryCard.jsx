8e745e4b9bd3c9495e25af836dc2abcc6fb134df
praharsh sharma <pshweb0310@gmail.com>
2024-11-25 02:58:42 +0530

    first commit

--- src/components/PurchaseHistoryCard.jsx
+++ src/components/PurchaseHistoryCard.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardContent, Typography, Divider, Box } from "@mui/material";
 import { getproduct } from "../api/api";
 
 const PurchaseHistoryCard = ({ purchase }) => {
+
+  const getProductDetail = async (id)=>{
+    const res = await getproduct(id)
+    console.log(res);
+
+    setnames(name+" "+res.product.name+" ")
+  }
+
+  const [name,setnames] = useState("");
+
+  useEffect(()=>{
+    console.log(purchase.products.length)
+    for(let i=0;i<purchase.products.length;i++)
+    {
+      getProductDetail(purchase.products[i].productId)
+      setnames(name+"x"+purchase.products[i].quantity)
+    }
+    
+  },[])
   return (
     <Card
       sx={{ width: "100%", boxShadow: 3, borderRadius: 2, marginBottom: 2 }}
@@ -15,20 +34,18 @@ const PurchaseHistoryCard = ({ purchase }) => {
 
         <Box>
           <Typography variant="body2" color="text.secondary">
-            <strong>Product ID:</strong> {purchase._id}
+            <strong>Products:</strong> {name
+            }
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <strong>Product Name:</strong> {purchase.productName}
+            <strong>Address:</strong> {purchase.address}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            <strong>Quantity:</strong> {purchase.quantity}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            <strong>Price:</strong> ${purchase.price}
+            <strong>Amount:</strong> Rs{purchase.totalAmount}
           </Typography>
           <Typography variant="body2" color="text.secondary">
             <strong>Purchase Date:</strong>{" "}
-            {new Date(purchase.purchaseDate).toLocaleDateString()}
+            {new Date(purchase.updatedAt).toLocaleDateString()}
           </Typography>
         </Box>
       </CardContent>
